6b154d27d37998b4b4a97df4a69c3721aafa4b98
Inkmptnz <adrian.peikert3@gmail.com>
2023-09-07 01:04:31 +0200

    created liar and betting gamelogic

--- liarsDice.py
+++ liarsDice.py
@@ -3,13 +3,14 @@ import random
 class Player:
     current_dices = []
     current_dice_count = 5
+    bets = []
 
     def __init__(self, name, order):
         self.name = name
         self.order = order
     
     def __str__(self):
-        return f"Name: {self.name}, Order: {self.order}, Dices: {str(self.current_dices)}"
+        return f"Name: {self.name}, Order: {self.order}, Dices: {str(self.current_dices)}, Dice count: {self.current_dice_count}"
     
     def __lt__(self, other):
         return self.order < other.order
@@ -28,9 +29,9 @@ def set_dices(players, count_dices):
 def random_dices(count_dices):
     return [random.randint(1,6) for _ in range(count_dices)]
 
-def print_player(player):
-    for i in player:
-        print(i)
+def print_players(players):
+    for player in players:
+        print(player)
 
 def count_dices(players):
     count_dices = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
@@ -46,19 +47,70 @@ def start_game(player_count, start_dice_count):
     players = create_players(player_count)
     set_dices(players, start_dice_count)
     sorted(players)
-
-    rounds = []
-    no_liar = True
+    
+    called_liar = False
     i = 0
     first_bet = True
-    while no_liar:
+    last_bet = (-1,-1)
+    bets = []
+    while True:
+        current_player = i%player_count
+        print(players[current_player])
         if (first_bet):
-            print(dice_options(-1, player_count, start_dice_count))
-        break
+            last_bet = make_a_bet()
+            players[current_player].bets.append(last_bet)
+            bets.append(last_bet)
+            first_bet = False
+            i += 1
+            continue
+            
+        print("The last bet was: ", last_bet[0], ", ", last_bet[1])
+
+        while True:
+            anwser = input("Do you call a lie? y/n \n")
+            if anwser == "y":
+                called_a_liar(players[current_player - 1], players[current_player], count_dices(players))
+                called_liar = True
+                print_players(players)
+                break
+            elif anwser == "n":
+                break
+            else:
+                print("You have to anwser either y or n")
+        if called_liar:
+            break
+
+        last_bet = make_a_bet()
+        players[current_player].bets.append(last_bet)
+        bets.append(last_bet)
+        i += 1
+        
+def called_a_liar(accused, caller, count_dices):
+    print(count_dices)
+    accused_bet = accused.bets[-1]
+    if count_dices[accused_bet[1]] >= accused_bet[0]:
+        print("The accuser is WRONG! Wrong accusations lead to a loose of a dice.")
+        caller.current_dice_count -= 1
+    else:
+        print("He lied therefore he has to sacrifice a dice!")
+        accused.current_dice_count -= 1
+    
+
+def make_a_bet():
+    last_bet = (-1, -1)
+    while True:
+        try:
+            last_bet = [int(x) for x in input("Make a bet with format: dice_count,dice\n").split(",")]
+            print("Your bet is: ", last_bet[0], ", ", last_bet[1])
+            break
+        except:
+            print("The Input is invalid. Either you didn't used the comma incorrectly or you didnt give me two numbers. (Correct example: 1,2)")
+    return last_bet
+            
 
 def dice_options(last_bet, player_count, start_dice_count):
     dice_permutation = []
-    if last_bet == -1:
+    if last_bet == (-1,-1):
         for count in range(1, player_count*start_dice_count + 1):
             for dice in range(1, 7):
                 dice_permutation.append((count, dice))
@@ -72,7 +124,7 @@ def dice_options(last_bet, player_count, start_dice_count):
             dice_permutation.append((count, dice))
     return dice_permutation
 
-
+start_game(4,5)
 
 
 
